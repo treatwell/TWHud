b68d5393868559c166f03fc683e8fe2e179bd65c
Marius <marius@wahanda.com>
2020-01-09 14:24:58 +0200

    Added project file for Carthage

---
+++ TWHud/TWHud.h
@@ -0,0 +1,19 @@
+//
+//  TWHud.h
+//  TWHud
+//
+//  Created by Marius Kazemekaitis on 2020-01-09.
+//  Copyright © 2020 Treatwell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TWHud.
+FOUNDATION_EXPORT double TWHudVersionNumber;
+
+//! Project version string for TWHud.
+FOUNDATION_EXPORT const unsigned char TWHudVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TWHud/PublicHeader.h>
+
+
